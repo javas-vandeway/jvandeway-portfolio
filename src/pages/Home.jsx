6c4579930416fde10b363c9602369e2bc4fe5259
javas <jvandeway@gmail.com>
2026-03-07 18:23:21 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,6 +19,7 @@ export default function Home() {
         <ExperienceSection />
         <SkillsSection />
         <BeyondSection />
+        <CareerTimeline />
         <ContactSection />
       </div>
     </ThemeProvider>
